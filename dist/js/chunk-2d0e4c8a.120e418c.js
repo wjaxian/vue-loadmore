3fe6810f34d042e5a9ff845224292f1935b1a927
吴佳显 <523453335@qq.com>
2019-04-29 17:33:27 +0800

    dist

---
+++ dist/js/chunk-2d0e4c8a.120e418c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e4c8a"],{9261:function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("section",[n("router-link",{attrs:{to:"/mobile"}},[e._v("Mobile")]),n("br"),n("router-link",{attrs:{to:"/pc"}},[e._v("PC")])],1)},o=[],l={name:"index"},c=l,i=n("2877"),u=Object(i["a"])(c,r,o,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0e4c8a.120e418c.js.map
