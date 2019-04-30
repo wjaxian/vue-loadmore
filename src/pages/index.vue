c7eef3d46e3939e7710fc46cc6d1d60213158b6a
吴佳显 <523453335@qq.com>
2019-04-30 08:31:07 +0800

    优化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-<section>
+<section class="index">
+	<img src="../assets/logo.png"/>
+	<h3>loadmore demo</h3>
 	<router-link to="/mobile">Mobile</router-link>
-	<br/>
 	<router-link to="/pc">PC</router-link>
 </section>
 </template>
@@ -11,3 +12,13 @@ export default {
 	name: 'index'
 }
 </script>
+
+<style lang="scss">
+.index {
+	text-align: center;
+	a {
+		margin-top: 10px;
+		display: block;
+	}
+}
+</style>
